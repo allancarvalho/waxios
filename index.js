fef7e5ddfea67645f366734963733528aaf2adab
Allan de carvalho <allan.talk@gmail.com>
2016-06-08 00:39:51 -0300

    all methods from axios

--- index.js
+++ index.js
@@ -2,16 +2,14 @@
 
 const axios = require('axios');
 const retry = require('retry-promise').default;
-const axiosMethods = ['get', 'post'];
+const axiosMethods = ['get', 'post', 'delete', 'head', 'put', 'patch'];
 const methods = {};
 
 axiosMethods.forEach((method) => {
     return methods[method] = function() {
         let args = Array.prototype.slice.call(arguments);
         let retries = args[1] && args[1].retry ? args[1].retry : 3;
-        return retry({ max: retries, backoff: 1000 }, function (attempt) {
-            return axios[method].apply(this, args)
-        });
+        return retry({ max: retries, backoff: 1000 }, () =>  axios[method].apply(this, args));
     };
 });
 
